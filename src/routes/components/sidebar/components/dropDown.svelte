4de1cf887637c32019e374e4a35f534b6a96b919
Aidan <aidanmarler1@gmail.com>
2025-05-29 16:20:27 -0600

    Added Plus/Minus button to dropdown
    To dropdown component, added a plus/minus button

--- src/routes/components/sidebar/components/dropDown.svelte
+++ src/routes/components/sidebar/components/dropDown.svelte
@@ -11,7 +11,7 @@
 <div
 	class="flex-column justify-left mb-4 flex flex h-full items-center items-center border-y-[4px]"
 >
-	<div class=" mr-1 flex bg-neutral-200 px-2">
+	<div class=" flex bg-neutral-200 px-2">
 		<h3 class=" font-semibold">{category == 'Country' ? 'Countries' : 'Contacts'}</h3>
 		<button
 			class="invert"
@@ -21,16 +21,27 @@
 				dropdownOpen = !dropdownOpen;
 			}}
 		>
-			<img alt="Arrow" src="icons/interaction/arrowLeft.svg" />
+			{#if dropdownOpen}
+				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+					<path
+						fill="#fff"
+						d="M13 6a1 1 0 1 0-2 0v5H6a1 1 0 1 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5z"
+					/>
+				</svg>
+			{:else}
+				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+					<path fill="#fff" d="M5 12a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1" />
+				</svg>
+			{/if}
 		</button>
 	</div>
 	<div class="overflow-hidden">
 		{#if dropdownOpen}
-			<div class=" z-10 flex flex-wrap space-x-1">
+			<div class="pl-1 z-10 flex flex-wrap space-x-1">
 				{#each items as item (item)}
 					<button
-						in:fly|global={{ x: -100, duration: 300, delay: 100 }}
-						out:fly|global={{ x: -100, duration: 300 }}
+						in:fly|global={{ x: -100, duration: 300 }}
+						out:fly|global={{ x: -100, duration: 200 }}
 						title={'See ' + item}
 						class="bubble bg-neutral-500 bg-opacity-50 hover:bg-opacity-70"
 						aria-label={'See ' + item}
