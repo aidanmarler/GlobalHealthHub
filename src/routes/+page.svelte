23438b17753a8a47b3c1446ad050d52381397c0e
Aidan <aidanmarler1@gmail.com>
2025-07-28 14:57:17 +0200

    Removed test artical elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,10 @@
 <script>
-	import Test from './components/sidebar/test.svelte';
 	import GhHub from './ghHub.svelte';
 </script>
 
 <!-- Outer container mimicking the webpage -->
 <div class="mx-auto max-w-6xl bg-white p-4 md:w-[720px] lg:w-[940px] xl:w-[1140px]">
-	<!-- Fake article content -->
-	<article>
-		<div class="my-8 h-1 w-full bg-zinc-200"></div>
-		<h1 class="relative my-8 text-5xl font-bold">Fake Article Title</h1>
-		<p class="text-md relative">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-			consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
-			aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-			incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
-			adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-			ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
-			et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-			consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
-			aliqua.
-		</p>
-		<h3 class="relative mt-8 text-2xl font-semibold">Subheading</h3>
-		<p class="text-md relative my-3">
-			More placeholder content here. Quis ipsum suspendisse ultrices gravida dictum fusce ut.
-		</p>
-
-		<div class="my-8 h-1 w-full bg-zinc-200"></div>
-
-		<Test></Test>
-
-		<!-- Your app -->
-		<section class="relative my-8">
-			<div class="mx-auto h-full w-full max-w-full">
-				<GhHub />
-			</div>
-		</section>
-
-		<div class="my-8 h-1 w-full bg-zinc-200"></div>
-
-		<h2 class="relative my-8 text-3xl font-bold">Fake Article Title</h2>
-		<p class="text-md relative">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-			consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
-			aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-			incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
-			adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-			ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
-			et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-			eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-			consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna
-			aliqua.
-		</p>
-		<div class="my-8 h-1 w-full bg-zinc-200"></div>
-
-		<h3 class="relative mt-8 text-2xl font-semibold">Subheading</h3>
-		<p class="text-md relative my-3">
-			More placeholder content here. Quis ipsum suspendisse ultrices gravida dictum fusce ut.
-		</p>
-	</article>
+	<div class="mx-auto h-full w-full max-w-full">
+		<GhHub />
+	</div>
 </div>
