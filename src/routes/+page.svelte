c76f61f7cff14c88d31d798867588f4ec1a2a41c
Aidan <aidanmarler1@gmail.com>
2025-08-04 13:00:36 +0200

    Getting set up for iframe integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <!-- Outer container mimicking the webpage -->
-<div class="mx-auto max-w-6xl bg-white p-4 md:w-[720px] lg:w-[940px] xl:w-[1140px]">
+<!--<div class="mx-auto max-w-6xl bg-white p-4 md:w-[720px] lg:w-[940px] xl:w-[1140px]">-->
 	<div class="mx-auto h-full w-full max-w-full">
 		<GhHub />
 	</div>
-</div>
+<!--</div>-->
